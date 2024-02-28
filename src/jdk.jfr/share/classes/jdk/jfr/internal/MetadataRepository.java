9e19ea200b06683f9dca9e98eff3bae005a5d3b0
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2024-02-28 15:49:53 +0100

    Add 'select' setting support for JFR events

--- src/jdk.jfr/share/classes/jdk/jfr/internal/MetadataRepository.java
+++ src/jdk.jfr/share/classes/jdk/jfr/internal/MetadataRepository.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2016, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, Datadog, Inc. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -82,6 +83,9 @@ public final class MetadataRepository {
                     if (!pEventType.isMethodSampling()) {
                         PeriodicEvents.addJVMEvent(pEventType);
                     }
+                } else {
+                    // enable selector for all non-periodic events
+                    pEventType.setHasSelector(true);
                 }
                 nativeControls.add(new EventControl(pEventType));
                 nativeEventTypes.add(eventType);
